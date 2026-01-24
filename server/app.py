6497f4b8faa9ebe205d68a7762f1679f5df1a05c
Core-tech250 <goelnishant813@gmail.com>
2026-01-24 09:12:10 +0000

    calling api

--- server/app.py
+++ server/app.py
@@ -1,13 +1,20 @@
 from flask import Flask
 from flask_cors import CORS
-
+import requests 
 app = Flask(__name__)
 
 CORS(app)
-
+apikey="54343504-5c1b955440ea9c04bd682c0b3"
+urlvariable="https://pixabay.com/api/?key=54343504-5c1b955440ea9c04bd682c0b3&q=yellow+flowers&image_type=photo&pretty=true"
+response=requests.get(urlvariable)
+if response.status_code==200 :
+    data = response.json()
+else :
+    data=f"error {response.status_code}" 
 @app.route("/reconnect")
 def index():
-    return {'recomend':1}
+    return data
+
 
     # ... (previous code) ...
 
