a4c8cab0593b65f9884280ae673e7c72f0caa195
Bhavya Gupta <bhavyag1337@gmail.com>
2026-01-24 10:28:02 +0000

    foreign key use so claint can see the images properly

--- clint/src/App.jsx
+++ clint/src/App.jsx
@@ -1,24 +1,20 @@
 
 import './App.css'
 import Card from './components/card.jsx'
-import { useEffect,usestate } from 'react';
+import { useEffect,useState } from 'react';
 
 function App() {
-  const [Arr,setarr]=usestate([{}])
+  const [arr,setarr]=useState([])
   useEffect(()=>{
-    fetch("https://potential-space-waffle-jjgp9xj5pvqrhp7vq-5000.app.github.dev/reconnect")
+    fetch("https://stunning-succotash-pj966456x9rx26rw4-5000.app.github.dev/reconnect")
     .then(res=>res.json())
-    .then(data=>setarr(data))
+    .then(data=>setarr(data.hits))
     .catch(error=>console.error(error)
     )
-  })
-  const cardData = Array.from({ length: 10 }, (_, index) => ({
-    id: index,
-    title: `Card Title ${index + 1}`,
-    description: "This is a reusable React card component.",
-    image: "https://picsum.photos/300/200"
+  },[])
 
-  }));
+  console.log(arr)
+
 
   return (
     <div className="app">
@@ -42,12 +38,12 @@ function App() {
         </aside>        
         <main className="content">
           <div className="card-container">
-            {cardData.map((card) => (
+            {arr.map((card) => (
               <Card
                 key={card.id}
-                title={card.title}
-                description={card.description}
-                image={card.image}
+                title={card.user}
+                description={card.tags}
+                image={card.largeImageURL}
               />
             ))}
           </div>
